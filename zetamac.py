e08d93c7edd6c973d67835c1f01de5bd0591ce4f
Saul Munn <100555686+saulmunn@users.noreply.github.com>
2024-12-24 12:11:08 -0800

    bug fixes

--- zetamac.py
+++ zetamac.py
@@ -35,18 +35,15 @@ def set_settings(): # the "settings" menubar
     def set_duration():
         global game_duration
         desired_duration = int(input("\nHow long do you want the game to last? Type your answer in seconds, then hit Enter: ").strip())
-        print(f"Games will now last {desired_duration} seconds.\n")
-        next_action = input("Hit \"p\" to play the game, or \"s\" to keep fiddling around with settings.\n")
         game_duration = desired_duration
-        if next_action == "s":
-            set_settings()
-        elif next_action == "p":
-            run_test()
+        print(f"Games will now last {desired_duration} seconds.\n")
+        ready_to_play_q("Hit \"p\" to play, or \"s\" to fiddle around with the settings.")
     
+
     
     #function that asks the user which settings they want to change
     def desired_settings():
-        desired_settings_change = input("\n\n===\n\nWhich settings do you want to change?\n\n\"d\" — duration of the game\n\"p\" — stop messing around with settings and play the game\n\n")
+        desired_settings_change = input(f"\n\n===\n\nWhich settings do you want to change?\n\n\"d\" — duration of the game (currently {game_duration} seconds)\n\"p\" — stop messing around with settings and play the game ({game_duration} seconds)\n\n")
         if desired_settings_change == "d":
             set_duration()
         elif desired_settings_change == "p":
@@ -59,13 +56,23 @@ def set_settings(): # the "settings" menubar
     #   [ ] range of numbers
     #   [x] time duration
 
-
+#function that asks the user if they're ready to play
+def ready_to_play_q(message):
+    print(f"Current settings:\n- Game duration: {game_duration} seconds\n- [no other editable settings at this time]")
+    next_action = input(f"\n{message}\n")
+    if next_action == "s":
+        set_settings()
+    elif next_action == "p":
+        run_test()
+    else:
+        print("Oops, that wasn't a readable command. Try again.")
+        ready_to_play_q(message)
+        
 
 def run_test():
     print(f"\n\n=== {game_duration} second game ===\n\n")
     start_time = time.time()
     correct = 0
-    attempts = 0
     while True:
         elapsed = time.time() - start_time
         if elapsed >= game_duration:
@@ -82,32 +89,25 @@ def run_test():
                         break
                     elif elapsed >= game_duration:
                         break
-                attempts += 1
         except KeyboardInterrupt:
             break
-    ending_message(correct, attempts)
+    ending_message(correct)
 
 def start():
     print("\n\n===\n\nWelcome to the Zetaterm: Zetamac in the terminal.")
-    print(f"\nThe Arithmetic Game is a fast-paced speed drill where you're given {game_duration} seconds to solve as many arithmetic problems as you can.")
-    next_step = input("\n\nPress \"p\" to play, or \"s\" to adjust the settings.\n")
-    if next_step.strip() == "p":
-        run_test()
-    elif next_step.strip() == "s":
-        set_settings()
-    else:
-        print("Oops, that wasn't a readable command. Try again.")
-        start()
+    print(f"\nThe Arithmetic Game is a fast-paced speed drill where you're given {game_duration} seconds to solve as many arithmetic problems as you can.\n")
+    ready_to_play_q("Press \"p\" to play, or \"s\" to adjust the settings.\n")
 
 
-def ending_message(correct, attempts):
+def ending_message(correct):
     print("Time's up!")
-    print(f"You answered {correct} correctly out of {attempts} attempted.\n\\ {correct} POINTS! \\")
-    next_action = input("\nPress \"p\" to play again, or \"s\" to edit the settings.\n")
-    if next_action == 'p':
-        run_test()
-    elif next_action == 's':
-        set_settings()
+    print(f"\n\\ {correct} POINTS! \n")
+    ready_to_play_q("Press \"p\" to play again, or \"s\" to edit the settings.\n")
+    # next_action = input("\nPress \"p\" to play again, or \"s\" to edit the settings.\n")
+    # if next_action == 'p':
+    #     run_test()
+    # elif next_action == 's':
+    #     set_settings()
 
 
 if __name__ == "__main__":
